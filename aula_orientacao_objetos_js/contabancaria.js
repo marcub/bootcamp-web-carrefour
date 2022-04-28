a595f292574b71a369ab4b49a7b4c2ef0b8ef133
marcub <marcusferraz98@gmail.com>
2022-04-27 23:04:21 -0300

    inclusao de atividades de aulas

---
+++ aula_orientacao_objetos_js/contabancaria.js
@@ -0,0 +1,76 @@
+class ContaBancaria {
+    constructor (agencia,numero,tipo) {
+        this.agencia=agencia;
+        this.numero=numero;
+        this.tipo=tipo;
+        this._saldo=0;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo=valor;
+    }
+
+    sacar (valor) {
+        if (valor<=this._saldo) {
+            this._saldo=this._saldo-valor;
+            return this._saldo;
+        }
+        else {
+        return 'Saldo insuficiente!';
+        }
+    }
+
+    depositar(valor) {
+        this._saldo=this._saldo+valor;
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends ContaBancaria {
+    constructor (agencia,numero,cartaoCredito) {
+        super(agencia,numero);
+        this.tipo='corrente';
+        this._cartaoCredito = cartaoCredito;
+    }
+    
+    get cartaoCredito() {
+        return this._cartaoCredito;
+    }
+    
+    set cartaoCredito(valor) {
+        this._cartaoCredito=valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    constructor (agencia,numero) {
+        super(agencia,numero);
+        this.tipo='poupanca';
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria {
+    constructor (agencia,numero) {
+        super(agencia,numero);
+        this.tipo='universitaria';
+    }
+
+    sacar (valor) {
+        if (valor<500) {
+            if (valor<=this._saldo) {
+                this._saldo=this._saldo-valor;
+                return this._saldo;
+            }
+            else {
+            return 'Saldo insuficiente!';
+            }
+        }
+        else {
+            return 'O saque deve ser menor que R$ 500,00'
+        }
+    }
+}
